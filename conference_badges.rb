27150a867d6463ef9739991075effc1e501763e8
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-23 18:07:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,5 @@
 def badge_maker(name)
-  "Hello, my name is #{name}."
-  
+  "Hello, my name is #{name}." #returning this
   
 end
 
@@ -8,8 +7,7 @@ def batch_badge_creator(people)
   new_array = []
   
   people.each do |name|
-    batch_maker(name)
-    
+    new_array << batch_maker(name)
   end
   
 new_array
