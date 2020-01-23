a117ea8d3d2ae4f925f639d5e9c6458ae8253ad1
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-23 19:01:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,9 +23,11 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
-  puts badge
-end
-  assign_rooms(array)
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
 
 
